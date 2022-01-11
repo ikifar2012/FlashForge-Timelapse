1dbf8c7d40a0c60639cc9808e02d548573540ccb
Matheson Steplock <ikifar2012@users.noreply.github.com>
2022-01-10 23:13:38 -0500

    init

---
+++ timelapse.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+function green { echo -e "\e[32m$*\e[39m"; }
+function red { echo -e "\e[31m$*\e[39m"; }
+export $(grep -v '^#' .env | xargs -d '\n')
+
+if [ -z "$printerip" ]; then
+    green "Enter Printer IP Address: "
+    read printerip
+fi
+
+green "Enter the ammount of time you want the timelapse to run for: "
+read printtime
+green "Enter the ammount of time between images: "
+read interval
+green "Enter the print name (folder will be created): "
+read printname
+mkdir $printname
+
+est_time=$(date -d "$printtime" "+%s")
+green "Starting at: $(date)"
+red "Ending at: $(date -d "$printtime")"
+green "Enter starting image number: "
+read num
+green "Starting capture at: $(date) on $printerip"
+green "Press enter to start capture"
+read
+while [ $(date "+%s") -lt ${est_time} ]; do
+    green "Capturing image: $num"
+    ffmpeg -i http://$printerip:8080/?action=stream -f image2 -frames:v 1 ${printname}/${printname}-${num}.png
+    sleep ${interval}
+    num=$((num+1))
+done
+green "Finished at: $(date)"
